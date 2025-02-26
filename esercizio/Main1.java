f1deddac3cd863094cecf36a3a6e6973657f3126
RivaGiorgia <riva.giorgia@itisgrassi.edu.it>
2025-02-26 11:54:43 +0100

    boh

--- esercizio/Main1.java
+++ esercizio/Main1.java
@@ -1,9 +1,9 @@
 public class Main1 {
     public static void main(String[] args) {
-        Rettangolo rettangolo = new Rettangolo(5, 3);
+        Rettangolo rettangolo = new Rettangolo(8, 4);
         System.out.println("Rettangolo - Area: " + rettangolo.area());
         System.out.println("Rettangolo - Perimetro: " + rettangolo.perimetro());
-        Quadrato quadrato = new Quadrato(4);
+        Quadrato quadrato = new Quadrato(5);
         System.out.println("Quadrato - Area: " + quadrato.area());
         System.out.println("Quadrato - Perimetro: " + quadrato.perimetro());
     }
